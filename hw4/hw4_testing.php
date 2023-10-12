61566c2ceb6a9f72f8e87ba066e188133c2597d5
suuuuu017 <suuuuu0177@outlook.com>
2023-10-11 21:27:20 -0400

    add test for hw4 testing

--- hw4/hw4_testing.php
+++ hw4/hw4_testing.php
@@ -30,7 +30,26 @@ $list2 = ["user" => "Wilma", "list" => ["bread", "apples", "coffee"]];
 print_r(count($list1["list"]));
 echo"\n";
 echo print_r(combineShoppingLists($list1, $list2));
+
+echo validateEmail("orange@virginia.edu"); // returns true
+echo "\n";
+echo validateEmail("no-reply@google.com"); // returns true
+echo "\n";
+echo validateEmail("orange.and.+blue@virginia.edu"); // returns true
+echo "\n";
+echo validateEmail("google.com"); // returns false
+echo "\n";
+echo validateEmail("mst3k@virginia.edu", "/^[a-z][a-z][a-z]?[0-9][a-z][a-z]?[a-z]?@virginia.edu$/"); // returns true
+echo "\n";
+echo validateEmail("orange@virginia.edu", "/^[a-z][a-z][a-z]?[0-9][a-z][a-z]?[a-z]?@virginia.edu$/"); // returns false
+echo "\n";
+echo validateEmail("orange@blue.com", "/^[a-z\.@]+$/"); // returns true
+echo "\n";
+echo validateEmail("orangeblue.com", "/^[a-z\.@]+$/"); // returns false (but matches this regex)
+echo "\n";
+echo validateEmail("orange123@blue.com", "/^[a-z\.@]+$/"); // returns false
 ?>
+
 
 <p>...</p>
 </body>
